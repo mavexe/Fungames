a98dc91fa20f2150a1db3cb241d8aefd13a161ad
Damir Mustafin <82767270+mavexe@users.noreply.github.com>
2024-08-20 15:34:01 +0600

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
-#include <string.h>
+#include <stdio.h>
 #include <vector>
-#include <unordered_map>
-#include "stdio.h"
+
+class Point {
+
+    Point(){
+    printf("Its conctructor");
+    }
+
+    void print(){
+        printf("its method");
+    }
+};
 
 int main(){
-    std::unordered_map<string, float> PLayers = {
-        {"Vitaliy",0.352},
-        {"Ilgiz",0,725},
-        {"Nikita Dallas Lock",0,407},
-        {"Damir",1,005},
-        {"Marsel",0,88},
-        {"Marat",0,66},
-        {"Usatiy",0,69},
-        {"Jacob", 0,68}
-    };
-    pairPLayers(PLayers);
-}
-
-void pairPLayers(const std::unordered_map <string,float&> playerweights){
-    std::vector<std::pair<string, float>> players;
-    for(const auto& player:playerWeights){
-    player.emplace_back(player);
-    }
-
-    sort(player.begin(), players.end(), 
-            [](const pair<string, float>& a, const pair<string, float>& b) {
-            return a.second<b.second;
-           });
-
-    std::cout<<"Team 1:"<<endl;
-    for(size_t i = 0; i<players.size()/2;++i){
-    std::cout<<"Player"<<players[i].first<<"with weight"<<players[i].second<<endl;
-        std::cout<<"Player"<<players[players.size()-1-i].first
-            <<"with weight"<<players[players.size()-1-i].second<<endl;
-    }
+    Point point1;
+    point1.print();
 
 }
