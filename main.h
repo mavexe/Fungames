683f697b7d88b0e792fcb7600ed79b2a412e06f3
Damir Mustafin <82767270+mavexe@users.noreply.github.com>
2024-10-14 10:46:12 +0500

    Add files via upload

--- main.h
+++ main.h
@@ -1,17 +1,2 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-class Point {
-    public:
-        Point(double x, double y);
-        ~Point();
-    int getX(int x){
-        return x;
-    }
-        void print() const;
-    private:
-        double x;
-        double y;        
-};
-
-#endif
+void SysTick_Timer_main(void);
